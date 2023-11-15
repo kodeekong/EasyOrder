52fc0d2c95c1dddccbebbe08477f6ec70812b4d7
Kodee <kodeekong@yahoo.com>
2023-11-15 11:00:28 -0500

    first commit

---
+++ script.js
@@ -0,0 +1,21 @@
+function signup(){
+    let email = document.getElementById('email').value 
+    let password = document.getElementById('password').value 
+
+    localStorage.setItem(email, password)
+    location.replace('index.html')
+}
+
+function login(){
+    let email = document.getElementById('email').value 
+    let password = document.getElementById('password').value 
+
+    if(localStorage.getItem(email)){
+        if(password === localStorage.getItem(email)){
+            location.replace('menu.html')
+        }
+    }
+    else{
+        alert('Incorrect Password')
+    }
+}
