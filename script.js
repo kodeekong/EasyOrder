a3421ca0466eb827586c1a29b8f85bb34d88bf1a
Kodee <kodeekong@yahoo.com>
2023-11-16 10:47:08 -0500

    log in page

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
+const container = document.getElementById('container');
+const registerBtn = document.getElementById('register');
+const loginBtn = document.getElementById('login');
+
+registerBtn.addEventListener('click', () => {
+    container.classList.add("active");
+});
+
+loginBtn.addEventListener('click', () => {
+    container.classList.remove("active");
+});
+
 function signup(){
     let email = document.getElementById('email').value 
     let password = document.getElementById('password').value 
@@ -12,7 +24,7 @@ function login(){
 
     if(localStorage.getItem(email)){
         if(password === localStorage.getItem(email)){
-            location.replace('menu.html')
+            alert('correct')
         }
     }
     else{
