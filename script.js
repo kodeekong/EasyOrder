b530e47cf60263ee2becdb2948aa375ac831fb2a
Kodee <kodeekong@yahoo.com>
2023-12-01 07:46:38 -0500

    ignore

--- script.js
+++ script.js
@@ -31,25 +31,23 @@ function signup(){
     localStorage.setItem(email, password)
 }
 
-const searchInput = document.getElementById('searchInput')
-const searchButton = document.getElementById('searchButton')
-const fooditems = document.getElementById('food-items')
-const details = document.querySelector('details')
-const detailssub = document.getElementById('details-sub')
-
 function search(){
-    var input, filter, title, item;
-    input = document.getElementById('searchInput');
-    filter = input.valuetoUpperCase();
-    title = document.getElementById('food-items');
-    item = document.getElementsByClassName('food-title');
+    const searchbox = document.getElementById('searchInput').value.toUpperCase();
+    const storeItem = document.getElementById('menu-container')
+    const product = document.querySelectorAll('food-items')
+    const pname = document.getElementsByTagName('h5')
 
-    for (i = 0 ; i< item.length; i++){
-        a = title[i].getElementsByTagName('h5')[0];
-        if (a.innerHTML.toUpperCase().indexOf(filter) > -1){
-            item[i].style.display = "";
-        } else {
-            item[i].style.display= "none";
+    for( var i = 0; i < pname.length; i++){
+        let match = product[i].getElementsByTagName('h5')[0];
+
+        if(match){
+           let textValue = match.textContent || match.innerHTML
+
+           if(textValue.toUpperCase().indexOf(searchbox) > -1) {
+            product[i].style.display = "";
+            } else {
+            product[i].style.display = "none"
+                }
+            }
         }
     }
-}
